ba89de149615e56f522bdfda8a15e2b849ea1cc9
Sumaxi <max@podolski.org>
2018-11-11 18:11:04 -0500

    Pricing

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Field, FieldLabel, FieldBody, Label, Control, Input, Select, Button} from "bloomer";
+import { Field, FieldLabel, FieldBody, Label, Control, Input, Select, Button, Columns, Column, Title, Subtitle } from "bloomer";
 
 export default class Form extends React.Component {
   constructor() {
@@ -7,7 +7,7 @@ export default class Form extends React.Component {
     this.state = {
       name: '',
       drinks: [{ name: '' }],
-      price: 0,
+      price: 4,
     };
   }
 
@@ -36,106 +36,120 @@ export default class Form extends React.Component {
   
   handleAddShareholder = () => {
     this.setState({ drinks: this.state.drinks.concat([{ name: '' }]) });
+    this.setState({ price: this.state.price + 4});
   }
   
   handleRemoveShareholder = (idx) => () => {
     this.setState({ drinks: this.state.drinks.filter((s, sidx) => idx !== sidx) });
+    this.setState({ price: this.state.price - 4});
   }
 
   render() {
     return (
       <form id="gform" method="GET" onSubmit={this.handleSubmit} action="https://script.google.com/macros/s/AKfycbzv-nXMAzhNAZdP8STPPmJT1b1X4uJM6Dw-eIeMEkkZ2t_qR9aB/exec">
-        <Field isHorizontal>
-          <FieldLabel isNormal>
-            <Label>Enter your contact!</Label>
-          </FieldLabel>
-          <FieldBody>
-            <Control>
-              <Input type="text" name="Name" placeholder='Name' />
-            </Control>
-            <Control>
-              <Input type="text" name="Phone" placeholder='(111) 222-3333' />
-            </Control>
-          </FieldBody>
-        </Field>
-
-        <Field isHorizontal>
-          <FieldLabel isNormal>
-            <Label>Location</Label>
-          </FieldLabel>
-          <FieldBody>
-            <Control>
+        <Columns isVCentered>
+          <Column isSize={9}>
+            <Field isHorizontal>
+              <FieldLabel isNormal>
+                <Label>Enter your contact!</Label>
+              </FieldLabel>
+              <FieldBody>
                 <Control>
-                  <Input type="text" name="Place" placeholder='Room #, Building' />
+                  <Input type="text" name="Name" placeholder='Name' />
                 </Control>
-            </Control>
-          </FieldBody>
-        </Field>
-
-        <Field isHorizontal>
-          <FieldLabel isNormal>
-            <Label>Chose your drink!</Label>
-          </FieldLabel>
-          <FieldBody>
-
-          </FieldBody>
-        </Field>
-        <Field>
-          {this.state.drinks.map((drink, idx) => (
-          <FieldBody>
-            <Control>
-              <Select name={`Drink #${idx + 1}`}
-                placeholder={`Drink #${idx + 1}`}
-                value={drink.name}
-                onChange={this.handleShareholderNameChange(idx)}>
-                  <option>Milk Tea Hot</option>
-                  <option>Taro Hot</option>
-                  <option>Milk Tea Cold</option>
-                  <option>Taro Cold</option>
-              </Select>
-              <Button onClick={this.handleRemoveShareholder(idx)}>X</Button>
-            </Control>
-          </FieldBody>
-          ))}
-        </Field>
-        <Button onClick={this.handleAddShareholder}>Add Drink</Button>
-
-        <Field isHorizontal>
-          <FieldLabel isNormal>
-            <Label>Additional Information</Label>
-          </FieldLabel>
-          <FieldBody>
-            <Control>
                 <Control>
-                  <Input type="text" name="Special" placeholder='Time or special requests' />
+                  <Input type="text" name="Phone" placeholder='(111) 222-3333' />
                 </Control>
-            </Control>
-          </FieldBody>
-        </Field>
+              </FieldBody>
+            </Field>
 
-        <Field isHorizontal>
-          <FieldLabel isNormal>
-            <Label>How did you hear about us?</Label>
-          </FieldLabel>
-          <FieldBody>
-            <Control>
+            <Field isHorizontal>
+              <FieldLabel isNormal>
+                <Label>Location</Label>
+              </FieldLabel>
+              <FieldBody>
                 <Control>
-                  <Input type="text" name="Tracking" placeholder='...' />
+                    <Control>
+                      <Input type="text" name="Place" placeholder='Room #, Building' />
+                    </Control>
                 </Control>
-            </Control>
-          </FieldBody>
-        </Field>
+              </FieldBody>
+            </Field>
 
-        <Field isHorizontal>
-          <FieldLabel /> {/* empty for spacing */}
-          <FieldBody>
-              <Field>
-                  <Control>
-                      <Button type="submit">Submit</Button>
-                  </Control>
-              </Field>
-          </FieldBody>
-        </Field>
+            <Field isHorizontal>
+              <FieldLabel isNormal>
+                <Label>Chose your drink!</Label>
+              </FieldLabel>
+              <FieldBody>
+
+              </FieldBody>
+            </Field>
+            <Field>
+              {this.state.drinks.map((drink, idx) => (
+              <FieldBody>
+                <Control>
+                  <Select name={`Drink #${idx + 1}`}
+                    placeholder={`Drink #${idx + 1}`}
+                    value={drink.name}
+                    onChange={this.handleShareholderNameChange(idx)}>
+                      <option>Milk Tea Hot</option>
+                      <option>Taro Hot</option>
+                      <option>Milk Tea Cold</option>
+                      <option>Taro Cold</option>
+                  </Select>
+                  <Button onClick={this.handleRemoveShareholder(idx)}>X</Button>
+                </Control>
+              </FieldBody>
+              ))}
+            </Field>
+            <Button onClick={this.handleAddShareholder}>Add Drink</Button>
+
+            <Field isHorizontal>
+              <FieldLabel isNormal>
+                <Label>Additional Information</Label>
+              </FieldLabel>
+              <FieldBody>
+                <Control>
+                    <Control>
+                      <Input type="text" name="Special" placeholder='Time or special requests' />
+                    </Control>
+                </Control>
+              </FieldBody>
+            </Field>
+
+            <Field isHorizontal>
+              <FieldLabel isNormal>
+                <Label>How did you hear about us?</Label>
+              </FieldLabel>
+              <FieldBody>
+                <Control>
+                    <Control>
+                      <Input type="text" name="Tracking" placeholder='...' />
+                    </Control>
+                </Control>
+              </FieldBody>
+            </Field>
+
+            <Field isHorizontal>
+              <FieldLabel /> {/* empty for spacing */}
+              <FieldBody>
+                  <Field>
+                      <Control>
+                          <Button type="submit">Submit</Button>
+                      </Control>
+                  </Field>
+              </FieldBody>
+            </Field>
+        </Column>
+          <Column isSize={3}>
+            <Field>
+              <FieldLabel>
+                <Title>Price</Title>
+                <Subtitle>$ {this.state.price}</Subtitle>
+              </FieldLabel>
+            </Field>
+          </Column>
+        </Columns>
         <br/> <br/>
       </form>
     );
